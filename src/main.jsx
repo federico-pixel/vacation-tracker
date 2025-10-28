d966d85065eec692637b447cadc816e452b12b81
federico-pixel <federico@hostfully.com>
2025-10-28 12:54:49 -0400

    Delete src directory

--- src/main.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./simple-gate/App.jsx";
-import "./simple-gate/styles.css";
-
-createRoot(document.getElementById("root")).render(<App />);
